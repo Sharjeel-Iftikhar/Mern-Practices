c4ee22c8ae46bc5fd792d57625ea0787b328925a
Sharjeel-Iftikhar <isharjeel63@gmail.com>
2023-06-24 22:06:09 +0500

    Regular Stuff

---
+++ ReactHooks/reacthooks/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ClassCounter from './componets/classCounter';
+
+function App() {
+  return (
+    <div className="App">
+     <ClassCounter/>
+    </div>
+  );
+}
+
+export default App;
